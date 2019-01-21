3c7ace44cfeed4b8a4dcc5472db968083bafc800
Christoph Walsh <walshcb@bu.edu>
2019-01-21 21:39:17 +0100

    move js and css to separate files

---
+++ js.js
@@ -0,0 +1,32 @@
+function thero() {
+  var a = "Ró";
+  var b = "is";
+  var c = "ín";
+  e_string = a + b + c;
+  document.write(e_string);
+}
+function thechoff() {
+  var a = "Chri";
+  var b = "sto";
+  var c = "ph";
+  e_string = a + b + c;
+  document.write(e_string);
+}
+function rphostc() {
+  var a = "criostoir";
+  var b = "breathnach";
+  var c = "@";
+  var d = "gmail";
+  var e = ".com";
+  e_string = "<a href=\"ma" + "ilto:" + a + b + c + d + e + "\">Christoph</a>";
+  document.write(e_string);
+}
+function rphostr() {
+  var a = "roisin";
+  var b = "donnelly";
+  var c = "ireland@";
+  var d = "gmail";
+  var e = ".com";
+  e_string = "<a href=\"ma" + "ilto:" + a + b + c + d + e + "\">Róisín</a>";
+  document.write(e_string);
+}
